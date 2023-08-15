04a90d0f521efad84c5d92dea19a135816a14400
Shane Moran <scm.aiml@gmail.com>
2023-08-15 18:00:14 -0400

    verbiage

--- ML_Projects/Pytorch/image_segmentation/Unet/README.md
+++ ML_Projects/Pytorch/image_segmentation/Unet/README.md
@@ -1,7 +1,7 @@
 #### UNET Implementation
 
 - [Original Paper](https://arxiv.org/abs/1505.04597)
-- [Locally stored version](./resources/UNET_paper.pdf)**
+- [Locally stored version](./resources/UNET_paper.pdf)
 
 Acknowledgements
 - [aladdinpersson](https://github.com/aladdinpersson/Machine-Learning-Collection/tree/master/ML)
--- ML_Projects/Pytorch/image_segmentation/Unet/model.py
+++ ML_Projects/Pytorch/image_segmentation/Unet/model.py
@@ -1,10 +1,10 @@
 """
-Pytorch implementation of Unet
+Pytorch implementation of UNET
 
-This provides a simple implementation of the Unet architecture for semantic (image) segmentation.
+This provides a simple implementation of the UNET architecture for semantic (image) segmentation.
 
 Classes:
-    DoubleConv: A double convolutional layer with batch normalization and ReLU activation.
+    DoubleConv: A double convolution layer with batch normalization and ReLU activation.
 
 author: Shane Moran
 (c) 2023 Shane Moran. All rights reserved.
@@ -15,20 +15,22 @@ import torch.nn as nn
 import torch.nn.functional as FF
 
 class DoubleConv(nn.Module):
-    """ A double convolutional layer in the Unet architecture.
+    """ A double convolution layer in the Unet architecture.
     
-    A series of two convolutional layers with batch normalization followed by ReLU activation.
+    A series of two convolution layers with batch normalization followed by ReLU activation.
+    Default kernel, padding, and stride result in a 'same' convolution, with different number
+    of channels.
 
     Args:
         in_chan (int): The number input channels.
         out_chan (int): The number of output channels.
-        kernel_size (int): The size of the convolutional kernel.
-        stride (int): The stride of the convolutional kernel.
-        padding (int): The padding of the convolutional kernel.
+        kernel_size (int, optional): The size of the convolution kernel.
+        stride (int, optional): The stride of the convolution kernel.
+        padding (int, optional): The padding of the convolution kernel.
 
     Attributes:
-        conv1 (nn.Conv2d): The first convolutional layer.
-        conv2 (nn.Conv2d): The second convolutional layer.
+        conv1 (nn.Conv2d): The first convolution layer.
+        conv2 (nn.Conv2d): The second convolution layer.
         batchnorm1 (nn.BatchNorm2d): The first batch normalization layer.
         batchnorm2 (nn.BatchNorm2d): The second batch normalization layer.
         relu1 (nn.ReLU): The first ReLU activation layer.
@@ -42,17 +44,7 @@ class DoubleConv(nn.Module):
         stride: int = 1,
         padding: int = 1
     ) -> None:
-        """ Initializes the DoubleConv class
-
-        Initialize the DobuleConv class with the defined input and output channels. The kernel size, stride, and padding are all set to 3, 1, and 1 respectively for a same convolution.
-
-        Args:
-            in_chan (int): The number input channels.
-            out_chan (int): The number of output channels.
-            kernel_size (int, optional): The size of the convolutional kernel.
-            stride (int, optional): The stride of the convolutional kernel.
-            padding (int, optional): The padding of the convolutional kernel.
-        """
+        
         super(DoubleConv, self).__init__()
         self.conv1 = nn.Conv2d(
             in_chan,
@@ -64,11 +56,11 @@ class DoubleConv(nn.Module):
         self.batchnorm1 = nn.BatchNorm2d(out_chan)
         self.relu1 = nn.ReLU(inplace = True)
         self.conv2 = nn.Conv2d(
-            out_chan, 
-            out_chan, 
-            kernel_size, 
-            stride = stride, 
-            padding = padding
+        out_chan, 
+        out_chan, 
+        kernel_size, 
+        stride = stride, 
+        padding = padding
         )
         self.batchnorm2 = nn.BatchNorm2d(out_chan)
         self. relu2 = nn.ReLU(inplace = True)
@@ -93,47 +85,51 @@ class DoubleConv(nn.Module):
 
         return x
 
-class contracting(nn.Module):
-    """ The contracting poertion of UNET architecture
+class DownwardLayers(nn.Module):
+    """ The downward portion of UNET architecture
 
-    The contracting portion of the UNET architecture. This is a series of DobubleConv layers followed 
-    by maxpooling layer with decreasing img size and increasing number of channels.
+    The downward portion of the UNET architecture. This is a series of DoubleConv layers followed 
+    by max-pooling layer with decreasing img size and increasing number of channels.
 
     Args:
         channels (list[int]): List of channels for each layer in contracting portion.
     
     Attributes:
-        contracting_layers (nn.ModuleList): A list of DoubleConv blocks followed by maxpooling layers.
+        downward_layers (nn.ModuleList): A list of DoubleConv blocks followed by max-pooling layers.
 
     """
 
     def __init__(self, channels: list[int]) -> None:
-        super(contracting, self).__init__()
-        self.contracting_layers = nn.ModuleList()
+        super(DownwardLayers, self).__init__()
+        self.downward_layers = nn.ModuleList()
 
-        # Loop over all excpet last channel value
+        # Loop over all except last channel value
         for i in range(len(channels)-1):
-            self.contracting_layers.append(
+            self.downward_layers.append(
                 DoubleConv(channels[i], channels[i+1])
             )
 
             # Add MaxPool2d to all but last layer
             if i < (len(channels)-2):
-                self.contracting_layers.append(
+                self.downward_layers.append(
                     nn.MaxPool2d(kernel_size=2)
                 )
 
     def forward(self, x: torch.Tensor) -> list[torch.Tensor]:
-        """ Forward pass of the contracting layers
+        """ Forward pass of the downward layers
+
+        Forward pass of the downward layers of the UNET architecture. This will also return
+        the output list of tensors from each DoubleConv block that is used in concatenation
+        with the upward portion of the UNET architecture.
 
         Args:
             x (torch.Tensor): An input tensor representing image.
 
         Returns:
-            list[torch.Tensor]: A list of the output tensors from each DoubleConv block
+            list[torch.Tensor]: A list of the output tensors from each DoubleConv block.
         """ 
         outputs = []
-        for step in self.contracting_layers:
+        for step in self.downward_layers:
             x = step(x)
 
             if isinstance(step, DoubleConv):
@@ -141,52 +137,52 @@ class contracting(nn.Module):
         
         return outputs
     
-class expanding(nn.Module):
-    """ The expanding part of the UNET architecture
+class UpwardLayers(nn.Module):
+    """ The upward portion of the UNET architecture
 
-    The expanding part of the UNET architecture. This is a series of upcovnolutions and concatenations 
-    with corresponding layer of contracting, followed by DoubleConv.
+    The upward portion of the UNET architecture. This is a series of up-convolutions and concatenations 
+    with corresponding layer of the downward portion, followed by DoubleConv.
 
     Args:
         channels (list[int]): List of channels for each layer in the expanding portion.
     
     Attributes:
-        expanding_layers (nn.ModuleList): A list of up-convolutional blocks.
+        upward_layers (nn.ModuleList): A list of up-convolution blocks.
 
     """
     def __init__(self, channels: list[int]) -> None:
-        super(expanding, self).__init__()
-        self.expanding_layers = nn.ModuleList()
+        super(UpwardLayers, self).__init__()
+        self.upward_layers = nn.ModuleList()
 
         for i in range(len(channels)-1):
-            self.expanding_layers.append(
+            self.upward_layers.append(
                 nn.ConvTranspose2d(
                 channels[i], channels[i+1], 2, 2
                 )
             )
 
-            self.expanding_layers.append(
+            self.upward_layers.append(
                 DoubleConv(
                 channels[i], channels[i+1]
                 )
             )
     
-    def forward(self, x: torch.Tensor, contract_features: list[torch.Tensor]) -> torch.Tensor:
-        """ forward pass for the expanding portion of UNET
+    def forward(self, x: torch.Tensor, downward_features: list[torch.Tensor]) -> torch.Tensor:
+        """ forward pass for the upward portion of UNET
         
         Args:
             x (torch.Tensor): An input tensor.
-            contract_features (list[torch.Tensor]): List of tensor features from the contracting portion.
+            downward_features (list[torch.Tensor]): List of tensor features from the downward portion.
         
         Returns:
-            torch.tesnor: Output feature tensor. 
+            torch.Tensor: Output feature tensor. 
         """
 
-        for i, step in enumerate(self.expanding_layers):
+        for i, step in enumerate(self.upward_layers):
             # concat feature from contract layer of DoubleConv
             if isinstance(step, DoubleConv):
                 # tensor size batch,channel,H,W -> concat along chan
-                x = torch.cat([contract_features[i//2],x], dim=1)
+                x = torch.cat([downward_features[i//2],x], dim=1)
             
             # Apply the step
             x = step(x)
@@ -201,8 +197,8 @@ class UNET(nn.Module):
         channels (list[int]): A list of channels for the convolutions at each layer.
 
     Attributes:
-        contractor (contracting): The contracting layers of UNET.
-        expander (expanding): The expanding layers of UNET.
+        downward (DownwardLayers): The downward layers of UNET.
+        upward (UpwardLayers): The upward layers of UNET.
         output (nn.Conv2d): The final layer.
 
     Example:
@@ -215,9 +211,9 @@ class UNET(nn.Module):
             out_channels: int
     ) -> None:
         super(UNET,self).__init__()
-        self.contractor = contracting(channels=channels)
-        # Need to reverse order and ommit last
-        self.expander = expanding(channels=channels[::-1][:-1])
+        self.downward = DownwardLayers(channels=channels)
+        # Need to reverse order and omit last
+        self.upward = UpwardLayers(channels=channels[::-1][:-1])
         self.output = nn.Conv2d(channels[1],out_channels=out_channels, kernel_size=1)
     
     def forward(self, x: torch.Tensor) -> torch.Tensor:
@@ -228,8 +224,8 @@ class UNET(nn.Module):
 
         """
         # reverse order to pass into expanding layer
-        contract_features = self.contractor(x)[::-1]
-        x = self.expander(contract_features[0], contract_features[1:] )
+        downward_features = self.downward(x)[::-1]
+        x = self.upward(downward_features[0], downward_features[1:] )
         x = self.output(x)
 
         return x
@@ -241,7 +237,7 @@ if __name__ == "__main__":
     out_channels = 1
     model = UNET(channels=channel_list, out_channels=out_channels)
 
-    in_tensor = torch.randn(10,3,512,512)
+    in_tensor = torch.randn(1,3,256,256)
     print(type(in_tensor))
     out = model(in_tensor)
 
